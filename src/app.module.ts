b6bcfe24cee1905ba12e063864762dba1ee4dc34
khachatryanyuri <khachatryanyuri94@mail.ru>
2023-01-16 19:12:27 +0400

    first commit 2

---
+++ src/app.module.ts
@@ -0,0 +1,17 @@
+import { HttpModule } from '@nestjs/axios';
+import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { ProductModule } from './product/product.module';
+
+@Module({
+  imports: [
+    MongooseModule.forRoot('mongodb://localhost:27017/mongotestdb'),
+    ProductModule,
+    HttpModule,
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
