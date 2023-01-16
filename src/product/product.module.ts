01d9fddba848ff0ae6e5e3496bc2dfaa7bcbee35
khachatryanyuri <khachatryanyuri94@mail.ru>
2023-01-16 19:11:21 +0400

    first commit

---
+++ src/product/product.module.ts
@@ -0,0 +1,30 @@
+import { Module } from '@nestjs/common';
+import { ClientsModule, Transport } from '@nestjs/microservices';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ProductController } from './product.controller';
+import { Product } from './product.entity';
+import { ProductService } from './product.service';
+
+@Module({
+  imports: [
+    TypeOrmModule.forFeature([Product]),
+    ClientsModule.register([
+      {
+        name: 'PRODUCT_SERVICE',
+        transport: Transport.RMQ,
+        options: {
+          urls: [
+            'amqps://qlbsgowa:vPu_4vJElcVA7kIpy9r058GBQg8VtIXY@puffin.rmq2.cloudamqp.com/qlbsgowa',
+          ],
+          queue: 'main_queue',
+          queueOptions: {
+            durable: false,
+          },
+        },
+      },
+    ]),
+  ],
+  controllers: [ProductController],
+  providers: [ProductService],
+})
+export class ProductModule {}
